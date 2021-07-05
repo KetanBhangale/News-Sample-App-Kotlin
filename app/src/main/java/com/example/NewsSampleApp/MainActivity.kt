2dd0cf660c96c051afa50894e5376a3020b2ca45
Ketan Bhangale <ketub4@gmail.com>
2021-07-05 22:30:15 +0530

    removed API Key

--- app/src/main/java/com/example/NewsSampleApp/MainActivity.kt
+++ app/src/main/java/com/example/NewsSampleApp/MainActivity.kt
@@ -27,29 +27,9 @@ class MainActivity : AppCompatActivity(), INewsAdapter, ISuccessListener {
         context = this@MainActivity
 
 
-            newsDao = NewsDao(this)
+        newsDao = NewsDao(this)
         progressBar.visibility = View.VISIBLE
-            newsDao.getNews()
-       // adapter = NewsAdapter(newsDao.getNews(), this)
-//        val scope = CoroutineScope(Dispatchers.Main)
-//        var newsList:ArrayList<Articles> = newsDao.getNews()
-//        scope.launch {
-//            withContext(Dispatchers.IO) {
-//                newsDao.getNews(context)
-//                Log.i("news count", "${newsList.size}")
-//                Log.i("news UI work", "${newsList.size}")
-//            }
-//
-//        }
-//    GlobalScope.launch(Dispatchers.IO) {
-//        val newsList:ArrayList<Articles> = newsDao.getNews()
-//        Log.i("news count","${newsList.size}")
-//        withContext(Dispatchers.Main){
-//            Log.i("news UI work","${newsList.size}")
-//        }
-//    }
-
-
+        newsDao.getNews()
 
 
     }
@@ -62,29 +42,29 @@ class MainActivity : AppCompatActivity(), INewsAdapter, ISuccessListener {
         customTabsIntent.launchUrl(this, Uri.parse(article.url));
     }
 
-private fun  getNews(context: Context){
-    val news: Call<News> = NewsService.retrofitInstance.getHeadLines("in", 1)
-    Log.i("url","${news.request().toString()}")
-    news.enqueue(object: Callback<News> {
-        override fun onFailure(call: Call<News>, t: Throwable) {
-            Log.i("failed", "${t.message}")
-        }
-
-        override fun onResponse(call: Call<News>, response: Response<News>) {
-            Log.i("Success", "${response.body().toString()}")
-            val news: News? = response.body()
-            if (news != null) {
-
-               // adapter = NewsAdapter(news.articles, context)
-                recyclerView.layoutManager = LinearLayoutManager(context)
-                recyclerView.adapter = adapter
+    private fun getNews(context: Context) {
+        val news: Call<News> = NewsService.retrofitInstance.getHeadLines("in", 1)
+        Log.i("url", "${news.request().toString()}")
+        news.enqueue(object : Callback<News> {
+            override fun onFailure(call: Call<News>, t: Throwable) {
+                Log.i("failed", "${t.message}")
             }
-        }
-    })
-}
+
+            override fun onResponse(call: Call<News>, response: Response<News>) {
+                Log.i("Success", "${response.body().toString()}")
+                val news: News? = response.body()
+                if (news != null) {
+
+                    // adapter = NewsAdapter(news.articles, context)
+                    recyclerView.layoutManager = LinearLayoutManager(context)
+                    recyclerView.adapter = adapter
+                }
+            }
+        })
+    }
 
     override fun onNewsSuccess(news: ArrayList<Articles>) {
-        Log.i("news count in success","${news.size}")
+        Log.i("news count in success", "${news.size}")
         progressBar.visibility = View.GONE
         adapter = NewsAdapter(news, this)
         recyclerView.layoutManager = LinearLayoutManager(context)
